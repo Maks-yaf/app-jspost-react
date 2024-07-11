2e66ee739aa2a0f9a2a07c71f57fe4c5a789e628
Maks-yaf <maksim.yafimchyk@gamil.com>
2024-07-11 17:02:41 +0300

    PostList component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, {useState} from 'react';
 import './styles/App.css'
-import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
 
+    const [posts, setPosts] = useState([
+        { id: 1, title: 'Javascript', body: 'Description' },
+        { id: 2, title: 'Javascript 2', body: 'Description 2' },
+        { id: 3, title: 'Javascript 3', body: 'Description 3' },
+        { id: 4, title: 'Javascript 4', body: 'Description 4' },
+    ]);
+
+    const [posts2, setPosts2] = useState([
+        { id: 1, title: 'Java', body: 'Description' },
+        { id: 2, title: 'Java 2', body: 'Description 2' },
+        { id: 3, title: 'Java 3', body: 'Description 3' },
+        { id: 4, title: 'Java 4', body: 'Description 4' },
+    ]);
+
     return (
         <div className="App">
-            <PostItem post={{ id: 1, title: 'JavaScript', body: 'Description' }} />
-            <PostItem post={{ id: 2, title: 'JavaScript', body: '2' }} />
-            <PostItem post={{ id: 3, title: 'JavaScript', body: '3' }} />
-            <PostItem post={{ id: 4, title: 'JavaScript', body: '4' }} />
-            <PostItem post={{ id: 5, title: 'JavaScript', body: '5' }} />
+            <PostList posts={posts} title='Javascript posts' />
+            <PostList posts={posts2} title='Java posts' />
         </div>
     );
 }
