c8bd07386f1f4594059ff21b8064bac170d999a7
Maks-yaf <maksim.yafimchyk@gamil.com>
2024-07-11 16:12:11 +0300

    Claas component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React, {useState} from 'react';
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
 
 function App() {
 
-    const [likes, setLikes] = useState(0)
-
-    function increment() {
-        setLikes(likes + 1);
-    }
-
-    function decrement() {
-        setLikes(likes - 1);
-    }
-
     return (
         <div>
-            <h1>{likes}</h1>
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
+            <Counter />
+            <ClassCounter />
         </div>
     );
 }
