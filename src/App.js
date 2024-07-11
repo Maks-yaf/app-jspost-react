345c015a4e4e5239f0e743cc2f7cb6b54f334966
Maks-yaf <maksim.yafimchyk@gamil.com>
2024-07-11 16:38:39 +0300

    PostItem component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, {useState} from 'react';
-import Counter from "./components/Counter";
-import ClassCounter from "./components/ClassCounter";
+import './styles/App.css'
+import PostItem from "./components/PostItem";
 
 function App() {
 
     return (
-        <div>
-            <Counter />
-            <ClassCounter />
+        <div className="App">
+            <PostItem post={{ id: 1, title: 'JavaScript', body: 'Description' }} />
+            <PostItem post={{ id: 2, title: 'JavaScript', body: '2' }} />
+            <PostItem post={{ id: 3, title: 'JavaScript', body: '3' }} />
+            <PostItem post={{ id: 4, title: 'JavaScript', body: '4' }} />
+            <PostItem post={{ id: 5, title: 'JavaScript', body: '5' }} />
         </div>
     );
 }
