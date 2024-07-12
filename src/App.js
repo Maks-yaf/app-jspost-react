fa87a739eb3413c80c68cb4da2e0efc107c7850c
Maks-yaf <maksim.yafimchyk@gamil.com>
2024-07-12 12:29:25 +0300

    Remove Posts

--- src/App.js
+++ src/App.js
@@ -3,44 +3,36 @@ import './styles/App.css'
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
 
 function App() {
 
     const [posts, setPosts] = useState([
-        { id: 1, title: 'Javascript', body: 'Description' },
-        { id: 2, title: 'Javascript 2', body: 'Description 2' },
-        { id: 3, title: 'Javascript 3', body: 'Description 3' },
-        { id: 4, title: 'Javascript 4', body: 'Description 4' },
+        {id: 1, title: 'Javascript', body: 'Description'},
+        {id: 2, title: 'Javascript 2', body: 'Description 2'},
+        {id: 3, title: 'Javascript 3', body: 'Description 3'},
+        {id: 4, title: 'Javascript 4', body: 'Description 4'},
     ]);
 
-    const [title, setTitle] = useState('');
+    const createPost = (newPost) => {
+        setPosts( [...posts, newPost]);
+    }
 
-    const bodyInputRef = useRef();
-
-    const addNewPost = (e) => {
-        e.preventDefault()
-        console.log(title);
-        console.log(bodyInputRef.current.value);
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id));
     }
 
 
     return (
         <div className="App">
-            <form>
-                <MyInput
-                    value={title}
-                    onChange={e => setTitle(e.target.value)}
-                    type="text"
-                    placeholder="Post name"
-                />
-                <MyInput
-                    ref={bodyInputRef}
-                    type="text"
-                    placeholder="Post description"
-                />
-                <MyButton onClick={addNewPost}>Add Post</MyButton>
-            </form>
-            <PostList posts={posts} title='Javascript posts' />
+            <PostForm create={createPost} />
+            {posts.length
+                ?
+                <PostList remove={removePost} posts={posts} title='Javascript posts'/>
+                :
+                <h2 style={{textAlign: 'center'}}>
+                    No posts
+                </h2>}
         </div>
     );
 }
