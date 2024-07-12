c77fa09977a16d46b0cf86113be36b07e866546e
Maks-yaf <maksim.yafimchyk@gamil.com>
2024-07-12 14:00:04 +0300

    Sort Posts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, {useRef, useState} from 'react';
+import React, {useState} from 'react';
 import './styles/App.css'
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
 
     const [posts, setPosts] = useState([
-        {id: 1, title: 'Javascript', body: 'Description'},
-        {id: 2, title: 'Javascript 2', body: 'Description 2'},
-        {id: 3, title: 'Javascript 3', body: 'Description 3'},
-        {id: 4, title: 'Javascript 4', body: 'Description 4'},
+        {id: 1, title: '11', body: 'aa'},
+        {id: 2, title: '22 2', body: 'vvv 2'},
+        {id: 3, title: '33 3', body: 'bbb 3'},
+        {id: 4, title: '444 4', body: 'nnn 4'},
     ]);
+    const [selectorSort, setSelectorSort] = useState("");
 
     const createPost = (newPost) => {
         setPosts( [...posts, newPost]);
@@ -22,10 +22,25 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id));
     }
 
+    const sortPosts = (sort) => {
+        setSelectorSort(sort);
+        setPosts([...posts].sort( (a , b) => a[sort].localeCompare(b[sort])));
+    }
+
 
     return (
         <div className="App">
             <PostForm create={createPost} />
+            <hr  style={{margin:'15px 0px'}}/>
+            <MySelect
+                value={selectorSort}
+                onChange={sortPosts}
+                defaultValue='Sort by'
+                options={[
+                    {value: 'title', name: 'By name'},
+                    {value: 'body', name: 'By description'},
+                ]}
+            />
             {posts.length
                 ?
                 <PostList remove={removePost} posts={posts} title='Javascript posts'/>
