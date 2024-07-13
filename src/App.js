791325e197855727b7c25aedc071c71d7c4a136e
Maks-yaf <maksim.yafimchyk@gamil.com>
2024-07-13 11:42:31 +0300

    UseMemo + searchPost

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, {useState} from 'react';
+import React, {useMemo, useState} from 'react';
 import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
 
@@ -13,10 +14,23 @@ function App() {
         {id: 4, title: '444 4', body: 'nnn 4'},
     ]);
     const [selectorSort, setSelectorSort] = useState("");
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const sortedPosts = useMemo( () =>{
+        console.log('sorted function work now')
+        if (selectorSort) {
+            return [...posts].sort((a, b) => a[selectorSort].localeCompare(b[selectorSort]))
+        }
+        return posts;
+    }, [selectorSort, posts] );
 
     const createPost = (newPost) => {
-        setPosts( [...posts, newPost]);
+        setPosts([...posts, newPost]);
     }
+
+    const sortedAndSearchedPost = useMemo ( () => {
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery));
+    }, [searchQuery, sortedPosts ])
 
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id));
@@ -24,26 +38,32 @@ function App() {
 
     const sortPosts = (sort) => {
         setSelectorSort(sort);
-        setPosts([...posts].sort( (a , b) => a[sort].localeCompare(b[sort])));
     }
 
 
     return (
         <div className="App">
-            <PostForm create={createPost} />
-            <hr  style={{margin:'15px 0px'}}/>
-            <MySelect
-                value={selectorSort}
-                onChange={sortPosts}
-                defaultValue='Sort by'
-                options={[
-                    {value: 'title', name: 'By name'},
-                    {value: 'body', name: 'By description'},
-                ]}
-            />
-            {posts.length
+            <PostForm create={createPost}/>
+            <hr style={{margin: '15px 0px'}}/>
+            <div>
+                <MyInput
+                    value={searchQuery}
+                    onChange={e => setSearchQuery(e.target.value)}
+                    placeholder='Seacrh...'
+                />
+                <MySelect
+                    value={selectorSort}
+                    onChange={sortPosts}
+                    defaultValue='Sort by'
+                    options={[
+                        {value: 'title', name: 'By name'},
+                        {value: 'body', name: 'By description'},
+                    ]}
+                />
+            </div>
+            {sortedAndSearchedPost.length
                 ?
-                <PostList remove={removePost} posts={posts} title='Javascript posts'/>
+                <PostList remove={removePost} posts={sortedAndSearchedPost} title='Javascript posts'/>
                 :
                 <h2 style={{textAlign: 'center'}}>
                     No posts
