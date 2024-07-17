aa63de1c0d42556d4394451933c66c978112c8f2
Maks-yaf <maksim.yafimchyk@gamil.com>
2024-07-17 13:49:39 +0300

    loading posts

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import '../styles/App.css'
 import PostList from "../components/PostList";
 import PostForm from "../components/PostForm";
@@ -11,6 +11,8 @@ import Loader from "../components/UI/loader/Loader";
 import {useFetching} from "../hooks/useFetching";
 import {getPageCount} from "../utils/pages";
 import Pagination from "../components/UI/pagination/Pagination";
+import {useObserver} from "../hooks/useObserver";
+import MySelect from "../components/UI/select/MySelect";
 
 function Posts() {
     const [posts, setPosts] = useState([]);
@@ -20,13 +22,18 @@ function Posts() {
     const sortedAndSearchedPost = usePosts(posts, filter.sort, filter.query)
     const [limit, setLimit] = useState(10);
     const [page, setPage] = useState(1);
+    const lastElement = useRef();
 
     const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
         const response = await PostService.getAll(limit, page)
-        setPosts(response.data)
+        setPosts([...posts, ...response.data])
         const totalCount = response.headers["x-total-count"]
         setTotalPages(getPageCount(totalCount, limit));
     })
+
+    useObserver(lastElement, page< totalPages, isPostsLoading, () => {
+        setPage(page+1)
+    });
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
@@ -34,8 +41,8 @@ function Posts() {
     }
 
     useEffect(() => {
-        fetchPosts()
-    }, [page]);
+        fetchPosts(limit, page)
+    }, [page, limit]);
 
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id));
@@ -63,17 +70,32 @@ function Posts() {
                 filter={filter}
                 setFilter={setFilter}
             />
+            <MySelect
+                value={limit}
+                onChange={value => setLimit(value)}
+                defaultValue='number of elements on the page'
+                options={[
+                    {value: 5, name: '5'},
+                    {value: 10, name: '10'},
+                    {value: 25, name: '25'},
+                    {value: -1, name: 'All posts'},
+                ]}
+            >
+
+
+            </MySelect>
             {postError &&
                 <h1>Error ${postError}</h1>
             }
-            {isPostsLoading
-                ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><Loader/></div>
-                :
-                <PostList
-                    remove={removePost}
-                    posts={sortedAndSearchedPost}
-                    title='Javascript posts'
-                />}
+            <PostList
+                remove={removePost}
+                posts={sortedAndSearchedPost}
+                title='Javascript posts'
+            />
+            <div ref={lastElement} style={{height: 20, background: 'lightgray'}}></div>
+            {isPostsLoading &&
+                <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><Loader/></div>
+            }
             <Pagination
                 page={page}
                 changePage={changePage}
